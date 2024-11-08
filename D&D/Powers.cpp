0f4199b64e80440a86c87605dff3ebca9aeb9344
Caro-perezVal <a01665909@tec.mx>
2024-11-08 11:00:11 -0600

    Powers.cpp

--- D&D/Powers.cpp
+++ D&D/Powers.cpp
@@ -2,4 +2,21 @@
 // Created by stanl on 29/10/2024.
 //
 
+string name, string description
 #include "Powers.h"
+
+void Powers::setName(string name){
+  this->name=name;
+}
+
+string Powers::getName(){
+  return name;
+}
+
+void Powers::setDescription(string description){
+  this->description=description;
+}
+
+string Powers::getDescription(){
+  return name;
+}
